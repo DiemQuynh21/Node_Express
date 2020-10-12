0e4a44e4d3020fb5e748957f2df3959da5912f91
DiemQuynh <lediemquynh999@gmail.com>
2020-10-12 22:26:22 +0700

    Refactor code: create routes, app/controller folder

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000
 
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -24,16 +25,9 @@ app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resource\\views'));
 
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
+// caaus hinh route
+route(app);
 
-app.get('/search', (req, res) => {
-  res.render('search');
-});
-app.post('/search', (req, res) => {
-  res.send('');
-});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ src/routes/news.js
@@ -0,0 +1,10 @@
+var express = require('express');
+const router = express.Router();
+
+
+const newController = require('../app/controllers/newsController');
+
+router.use('/', newController.index);
+
+
+module.exports = router;
